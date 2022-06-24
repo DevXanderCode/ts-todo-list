0692f3f324fb28f891ee1fe24b7c1b6a598797da
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-24 21:15:33 +0100

    started working on the new todo component form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import { TodoList } from "./components";
+import { TodoList, NewTodo } from "./components";
 
 function App() {
   const todos = [{ id: "t1", text: "Finish the course" }];
 
   return (
     <div className="App">
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
